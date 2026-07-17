5e2a13249c49254d98af8a7e38adde47a07af4ca
sunidhicricopinion-cmd <sunidhi.cricopinion@gmail.com>
2026-07-17 14:41:55 +0530

    Update Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -11,15 +11,52 @@ Group by revenue desc;
 
 Q3.-------TOTAL TRANSCATION ?
 SELECT COUNT(TRANSACTION_ID)
-FROM customer_transcations
+FROM customer_transcations;
 
 Q4.--------AVG TRANSCATION VALUE?
 SELECT AVG(Amount)
-FROM customer_transcations
+FROM customer_transcations;
 
-Q4.--------MONTHLY  REVENUE?
+Q5.--------MONTHLY  REVENUE?
 SELECT SUM(Amount) AS REVENUE, MONTH(TRANSACTION_DATE)
-FROM customer_transcations
+FROM customer_transcations;
+
+Q6.-------- which region genertaed the highest revenue?
+select region , sum (AMOUNT) as revenue
+from customer_transcations
+GROUP BY region
+ORDER BY total_revenue DESC
+LIMIT 1;
+
+Q7.----------who are the top 5 customers by revenue?
+Select Customer_ID, sum(Amount) as revenue, 
+from  customer_transcations
+GROUP BY customer_id	
+ORDER BY total_revenue DESC
+LIMIT 5;
+
+Q8.-------- which  month  genertaed the highest revenue?
+select DATE_FORMAT(transaction_date,'%Y-%m') , sum (AMOUNT) as  total_revenue
+from customer_transcations
+GROUP BY DATE_FORMAT(transaction_date, '%Y-%m')
+ORDER BY total_revenue DESC
+LIMIT 1;
+
+Q9.-----Which customers made the highest single transaction?
+
+    SELECT
+    customer_id,
+    transaction_id,
+    amount
+FROM customer_transactions
+WHERE amount = (
+    SELECT MAX(amount)
+    FROM customer_transactions
+);
+	
+
+
+	
 	
 
 	
